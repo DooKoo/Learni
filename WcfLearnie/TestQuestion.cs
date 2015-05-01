b25d7088ddbf4af37722fd0b572435267a216252
Shevchuk Illya <illyai.shevchuk@gmail.com>
2015-05-02 00:17:41 +0300

    wcf without testing and pettern

---
+++ WcfLearnie/TestQuestion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WcfLearnie
+{
+    public class TestQuestion
+    {
+        public string Text { get; set; }
+        public string[] Answers { get; set; }
+        public int Correct { get; set; }
+    }
+}
